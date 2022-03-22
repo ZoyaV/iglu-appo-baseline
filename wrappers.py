git add fix rotation wrapper

wrappers.py
@@ -80,7 +80,7 @@ class ObsWrapper(Wrapper):
      #   print(obs)
         info['grid'] = obs['grid']
         info['agentPos'] = obs['agentPos']
-        #info['obs'] = obs['obs']
+        info['obs'] = obs['obs']
         return self.observation(obs, reward, done, info), reward, done, info
 
 
@@ -406,45 +406,6 @@ class RandomRotation(Wrapper):
             # vec = np.random.choice([1,-1])
             if self.vec == 1:
                 # action[mapping['camera']]=1
-                obs, reward, done, info = super().step(9)
-            else:
-                #  action[mapping['camera']]=0
-                obs, reward, done, info = super().step(10)
-        else:
-            obs, reward, done, info = super().step(action)
-        return obs, reward, done, info
-
-class RandomRotation(Wrapper):
-    def __init__(self, env):
-        super().__init__(env)
-        self.steps = 0
-        self.vec = np.random.choice([1, -1])
-        self.total_rots = np.random.choice(list(range(0,71,5)))
-
-    def reset(self):
-        self.steps = 0
-        self.vec = np.random.choice([1, -1])
-        self.total_rots = np.random.choice(list(range(0,71,5)))
-        return super().reset()
-
-    def step(self, action):
-        #         mapping = {
-        #             'forward': 0,
-        #             'back': 1,
-        #             'left': 2,
-        #             'right': 3,
-        #             'jump': 4,
-        #             'attack': 5,
-        #             'use': 6,
-        #             'camera': 7,
-        #             'hotbar': 8,
-        #         }
-        # action = [0,0,0,0,0,0,0,0,0]
-        self.steps += 1
-        if self.steps <= self.total_rots:
-            # vec = np.random.choice([1,-1])
-            if self.vec == 1:
-                # action[mapping['camera']]=1
                 obs, reward, done, info = super().step(7)
             else:
                 #  action[mapping['camera']]=0
@@ -452,6 +413,7 @@ class RandomRotation(Wrapper):
         else:
             obs, reward, done, info = super().step(action)
         return obs, reward, done, info
+
 
 def flat_action_space(action_space):
     if action_space == 'human-level':